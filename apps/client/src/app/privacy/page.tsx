14a86a8a64d67dca5bf1638ae7a9b6588add30a7
Praveen Bisht <prvnbist@gmail.com>
2024-04-18 15:49:38 +0530

    chore: privacy policy, tos

---
+++ apps/client/src/app/privacy/page.tsx
@@ -0,0 +1,156 @@
+'use client'
+
+import Link from 'next/link'
+import { IconArrowLeft } from '@tabler/icons-react'
+
+import { Button, Container, List, Space, Text, Title } from '@mantine/core'
+
+export default function Page() {
+	return (
+		<Container py={120}>
+			<Link href="/">
+				<Button variant="outline" color="dark.2" leftSection={<IconArrowLeft size={18} />}>
+					Home
+				</Button>
+			</Link>
+			<Space h={32} />
+			<Title order={2}>Privacy Policy</Title>
+			<Space h={16} />
+			<Text c="dimmed">
+				TrackSubs ("tracksubs.co", "we", "us", and/or "our") built the subscription tracking app
+				as a Freemium app. This SERVICE is provided at no cost and is intended for use as is.
+			</Text>
+			<Space h={16} />
+			<Text c="dimmed">
+				This page serves to inform visitors about my policies concerning the collection,
+				utilization, and disclosure of Personal Information should they choose to utilize my
+				Service.
+			</Text>
+			<Space h={16} />
+			<Text c="dimmed">
+				By opting to utilize my Service, you consent to the collection and utilization of
+				information in accordance with this policy. The Personal Information gathered is
+				utilized for the provision and enhancement of the Service. I pledge not to employ or
+				distribute your information to any party except as outlined in this Privacy Policy. The
+				terminologies employed in this Privacy Policy carry the same connotations as those in
+				our Terms and Conditions, accessible at Subs Tracker, unless explicitly defined
+				differently herein.
+			</Text>
+			<Space h={24} />
+			<Title order={3}>Information Collection and Use</Title>
+			<Space h={16} />
+			<Text c="dimmed">
+				To enhance your experience with our Service, we may ask you to provide certain
+				personally identifiable information, such as your Email ID, Profile Photo, and Full
+				Name, through Google OAuth Login initiated by you. Please note that the app may utilize
+				third-party services that collect information for identification purposes.
+			</Text>
+			<Space h={16} />
+			<List>
+				<List.Item c="dimmed">
+					<Link
+						target="_blank"
+						rel="noreferrer noopener"
+						href="https://policies.google.com/privacy?hl=en-US"
+					>
+						Google Analytics
+					</Link>
+				</List.Item>
+			</List>
+			<Space h={24} />
+			<Title order={3}>Log Data</Title>
+			<Space h={16} />
+			<Text c="dimmed">
+				I'd like to inform you that when you use my Service and encounter an error in the app, I
+				collect data and information, including Log Data, through third-party products on your
+				phone. This Log Data may comprise details such as your device's Internet Protocol ("IP")
+				address, device name, operating system version, app configuration when using my Service,
+				the time and date of your Service usage, and other pertinent statistics.
+			</Text>
+			<Space h={24} />
+			<Title order={3}>Cookies</Title>
+			<Space h={16} />
+			<Text c="dimmed">
+				Cookies are small data files commonly utilized as anonymous unique identifiers. They are
+				sent to your browser from websites you visit and stored in your device's internal
+				memory. While this Service does not explicitly utilize these "cookies," it may
+				incorporate third-party code and libraries that do for information collection and
+				service enhancement purposes. You have the choice to accept or decline these cookies and
+				can be notified when a cookie is being sent to your device. However, declining our
+				cookies may result in limited access to certain portions of this Service.
+			</Text>
+			<Space h={24} />
+			<Title order={3}>Service Providers</Title>
+			<Space h={16} />
+			<Text c="dimmed">
+				I may employ third-party companies and individuals due to the following reasons:
+			</Text>
+			<Space h={16} />
+			<List>
+				<List.Item c="dimmed">To facilitate our Service;</List.Item>
+				<List.Item c="dimmed">To provide the Service on our behalf;</List.Item>
+				<List.Item c="dimmed">To perform Service-related services; or</List.Item>
+				<List.Item c="dimmed">To assist us in analyzing how our Service is used.</List.Item>
+			</List>
+			<Space h={16} />
+			<Text c="dimmed">
+				I wish to inform users of this Service that third parties may have access to their
+				Personal Information. This access is necessary for them to carry out tasks assigned to
+				them on our behalf. However, they are bound by obligation not to disclose or utilize the
+				information for any other purpose.
+			</Text>
+			<Space h={24} />
+			<Title order={3}>Security</Title>
+			<Space h={16} />
+			<Text c="dimmed">
+				Your trust in providing us with your Personal Information is greatly valued, and we are
+				dedicated to employing commercially acceptable measures to safeguard it. However, it's
+				important to note that no method of transmission over the internet or electronic storage
+				is entirely secure and reliable. Therefore, while we endeavor to maintain the highest
+				standards of security, we cannot guarantee absolute security for your information.
+			</Text>
+			<Space h={24} />
+			<Title order={3}>Links to Other Sites</Title>
+			<Space h={16} />
+			<Text c="dimmed">
+				This Service might include links to other websites. Clicking on a third-party link will
+				redirect you to that site. Please be aware that these external sites are not operated by
+				me. Therefore, I strongly recommend reviewing the Privacy Policy of these websites. I do
+				not have control over, nor do I assume responsibility for, the content, privacy
+				policies, or practices of any third-party sites or services.
+			</Text>
+			<Space h={24} />
+			<Title order={3}>Children’s Privacy</Title>
+			<Space h={16} />
+			<Text c="dimmed">
+				I do not knowingly collect personally identifiable information from children. I
+				encourage all children to refrain from submitting any personally identifiable
+				information through the Application and/or Services. It is recommended that parents and
+				legal guardians monitor their children's Internet usage and assist in enforcing this
+				Policy by instructing their children not to provide personally identifiable information
+				through the Application and/or Services without permission. If you suspect that a child
+				has provided personally identifiable information to us through the Application and/or
+				Services, please contact us. Additionally, you must be at least 16 years of age to
+				consent to the processing of your personally identifiable information in your country
+				(in some countries, we may allow your parent or guardian to do so on your behalf).
+			</Text>
+			<Space h={24} />
+			<Title order={3}>Changes to This Privacy Policy</Title>
+			<Space h={16} />
+			<Text c="dimmed">
+				I reserve the right to update our Privacy Policy periodically. Therefore, it's
+				recommended that you review this page periodically for any changes. Any alterations will
+				be communicated by posting the updated Privacy Policy on this page.
+			</Text>
+			<Space h={16} />
+			<Text c="dimmed">This policy is effective as of 2024-05-01</Text>
+			<Space h={24} />
+			<Title order={3}>Contact Us</Title>
+			<Space h={16} />
+			<Text c="dimmed">
+				If you have any questions or suggestions regarding my Privacy Policy, please feel free
+				to contact me at <Link href="mailto:prvnbist@gmail.com">prvnbist@gmail.com</Link>.
+			</Text>
+		</Container>
+	)
+}
---
+++ apps/client/src/app/terms-of-service/page.tsx
@@ -0,0 +1,130 @@
+'use client'
+
+import Link from 'next/link'
+import { IconArrowLeft } from '@tabler/icons-react'
+
+import { Button, Container, List, Space, Text, Title } from '@mantine/core'
+
+export default function Page() {
+	return (
+		<Container py={120}>
+			<Link href="/">
+				<Button variant="outline" color="dark.2" leftSection={<IconArrowLeft size={18} />}>
+					Home
+				</Button>
+			</Link>
+			<Space h={32} />
+			<Title order={2}>Terms & Conditions</Title>
+			<Space h={16} />
+			<Text c="dimmed">
+				By downloading or utilizing the app, Chrome extension, and website, you automatically
+				agree to these terms. Therefore, it's essential to carefully read them before using the
+				app. You are not permitted to copy or alter the app, any of its components, or our
+				trademarks in any manner. Attempting to extract the source code of the app or
+				translating it into other languages, or creating derivative versions, is also
+				prohibited. The app itself, along with all trademarks, copyrights, database rights, and
+				other intellectual property rights associated with it, remain the property of TrackSubs
+				and their licensors.
+			</Text>
+			<Space h={16} />
+			<Text c="dimmed">
+				TrackSubs ("tracksubs.co", "we", "us", and/or "our") developed the TrackSubs app with a
+				commitment to ensuring its usefulness and efficiency. Hence, we retain the right to
+				modify the app or introduce charges for its services, at our discretion and for any
+				valid reason. Rest assured, we will never impose charges for the app or its services
+				without transparently communicating the specifics of what you're paying for.
+			</Text>
+			<Space h={16} />
+			<Text c="dimmed">
+				The TrackSubs app stores and processes personal data provided by you to deliver our
+				Service. Ensuring the security of your phone and access to the app is your
+				responsibility. Therefore, we advise against jailbreaking or rooting your phone, which
+				involves removing software restrictions imposed by the official operating system of your
+				device. Such actions may expose your phone to malware, viruses, or malicious programs,
+				compromise its security features, and potentially result in the TrackSubs app
+				malfunctioning or becoming inoperable.
+			</Text>
+			<Space h={16} />
+			<Text c="dimmed">
+				The app utilizes third-party services, each of which has its own set of Terms and
+				Conditions. You can find the links to the Terms and Conditions of these third-party
+				service providers below:
+			</Text>
+			<Space h={16} />
+			<List>
+				<List.Item c="dimmed">
+					<Link
+						target="_blank"
+						rel="noreferrer noopener"
+						href="https://policies.google.com/privacy?hl=en-US"
+					>
+						Google Analytics
+					</Link>
+				</List.Item>
+			</List>
+			<Space h={16} />
+			<Text c="dimmed">
+				It's important to note that there are certain circumstances for which tracksubs.co
+				cannot be held responsible. Some functionalities of the app necessitate an active
+				internet connection. This connection can be through Wi-Fi or provided by your mobile
+				network provider. However, tracksubs.co cannot assume responsibility if the app does not
+				function at full capacity due to the absence of Wi-Fi or depletion of your data
+				allowance.
+			</Text>
+			<Space h={16} />
+			<Text c="dimmed">
+				When using the app outside an area with Wi-Fi, it's important to remember that your
+				agreement with your mobile network provider still applies. This means you may incur
+				charges from your mobile provider for data usage while accessing the app, or other
+				third-party charges. By using the app, you accept responsibility for such charges,
+				including roaming data charges if you use the app outside your home territory (i.e.,
+				region or country) without disabling data roaming. If you are not the bill payer for the
+				device on which you're using the app, please ensure you have obtained permission from
+				the bill payer to use the app.
+			</Text>
+			<Space h={16} />
+			<Text c="dimmed">
+				Similarly, tracksubs.co cannot always assume responsibility for how you use the app.
+				It's your responsibility to ensure that your device remains charged. If your device runs
+				out of battery and you're unable to access the Service, tracksubs.co cannot accept
+				liability.
+			</Text>
+			<Space h={16} />
+			<Text c="dimmed">
+				Regarding tracksubs.co's responsibility for your use of the app, it's crucial to
+				understand that while we strive to maintain accuracy and timeliness, we rely on third
+				parties to furnish us with information to make it accessible to you. tracksubs.co cannot
+				be held liable for any loss, whether direct or indirect, resulting from your complete
+				reliance on this functionality of the app.
+			</Text>
+			<Space h={16} />
+			<Text c="dimmed">
+				At some juncture, we may contemplate updating the app. Currently, the app is accessible
+				on various platforms, and the system requirements (including any additional systems we
+				may choose to extend the app's availability to) could change. Consequently, you'll need
+				to download updates if you wish to continue using the app. Please note that tracksubs.co
+				does not guarantee continuous updates to ensure relevance or compatibility with the
+				version installed on your device. Nonetheless, you agree to accept updates to the
+				application when they are offered to you. Additionally, we reserve the right to cease
+				providing the app and may terminate its use at any time without prior notice to you.
+				Unless notified otherwise, upon termination: (a) the rights and licenses granted to you
+				in these terms will cease; (b) you must discontinue using the app and, if necessary,
+				remove it from your device.
+			</Text>
+			<Space h={24} />
+			<Title order={3}>Changes to This Terms and Conditions</Title>
+			<Space h={16} />
+			<Text c="dimmed">
+				TrackSubs reserves the right to update our Terms and Conditions periodically. Therefore,
+				we recommend reviewing this page periodically for any changes. Any alterations will be
+				communicated by posting the updated Terms and Conditions on this page.
+			</Text>
+			<Text c="dimmed">This policy is effective as of 2024-05-01</Text>
+			<Space h={16} />
+			<Text c="dimmed">
+				If you have any questions or suggestions regarding my terms of services, please feel
+				free to contact me at <Link href="mailto:prvnbist@gmail.com">prvnbist@gmail.com</Link>.
+			</Text>
+		</Container>
+	)
+}
